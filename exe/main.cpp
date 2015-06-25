26b525a5f1d207415daafe9bcf53743a69a1dada
Adrien Thebo <git@somethingsinistral.net>
2015-06-24 20:33:11 -0700

    hide executables from arduino

---
+++ exe/main.cpp
@@ -0,0 +1,45 @@
+#include "platform.h"
+
+#include "octo-driver.h"
+
+Cygni::OctoDriver *driver;
+
+void blink(int duration) {
+    digitalWrite(13, HIGH);
+    delay(duration);
+    digitalWrite(13, LOW);
+    delay(duration);
+}
+
+void pulse(int count, int duration) {
+    for(int i = 0; i < count; i++) blink(duration);
+}
+
+void setup() {
+    pinMode(13, OUTPUT);
+    pulse(5, 25);
+
+    driver = new Cygni::OctoDriver(50 * 8, 50);
+    driver->clear();
+    driver->show();
+}
+
+void loop() {
+    blink(100);
+
+    driver->set_pixel(1, 128, 0, 0);
+    driver->show();
+
+    delay(100);
+    driver->clear_pixel(1);
+    driver->show();
+    delay(100);
+}
+
+int main() {
+    setup();
+    while(true) {
+        loop();
+    }
+    return 0;
+}
