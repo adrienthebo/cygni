27679a866138d11034eaf738e0491a39a428d0d4
Adrien Thebo <git@somethingsinistral.net>
2015-06-24 18:23:01 -0700

    driver: add _length member variable

--- driver.h
+++ driver.h
@@ -3,10 +3,18 @@
 namespace Cygni {
     struct Driver {
 
+        Driver(int length) {
+            _length = length;
+        }
+
         virtual void show() = 0;
 
         virtual bool is_busy() {
             return false;
         }
+
+    protected:
+
+        int _length;
     };
 };
