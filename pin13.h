2172b67fa886e2bbd9f3cee47d514e2415719a79
Adrien Thebo <git@somethingsinistral.net>
2015-07-17 23:28:38 -0700

    add pin13.h

---
+++ pin13.h
@@ -0,0 +1,16 @@
+#pragma once
+
+void setup13() {
+    pinMode(13, OUTPUT);
+}
+
+void blink13(int duration) {
+    digitalWrite(13, HIGH);
+    delay(duration);
+    digitalWrite(13, LOW);
+    delay(duration);
+}
+
+void pulse13(int count, int duration) {
+    for(int i = 0; i < count; i++) blink13(duration);
+}
