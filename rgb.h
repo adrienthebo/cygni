41c2412de80c8d5f276c330e605d554dbf03fe36
Adrien Thebo <git@somethingsinistral.net>
2015-08-02 10:50:49 -0700

    rgb: add uint fade

--- rgb.h
+++ rgb.h
@@ -12,6 +12,12 @@ namespace Cygni {
             return red == other.red && green == other.green && blue == other.blue;
         }
 
+        void fade(uint8_t fade_by) {
+            red   -= fade_by;
+            green -= fade_by;
+            blue  -= fade_by;
+        }
+
         uint32_t to_int() {
             return (red << 16) | (green << 8) | blue;
         }
--- test/test_rgb.cpp
+++ test/test_rgb.cpp
@@ -7,3 +7,10 @@ SCENARIO("Converting to rgb int value") {
     RGB value = {0xF0, 0x7E, 0x0F};
     REQUIRE(value.to_int() == 0xF07E0F);
 }
+
+SCENARIO("Fading") {
+    RGB value = {0xF0, 0x10, 0x0B};
+    RGB expected = {0xE6, 0x06, 0x01};
+    value.fade(10);
+    REQUIRE(value == expected);
+}
