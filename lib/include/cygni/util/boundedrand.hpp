21093f86b6a775c70dbb113210eb1857cf96a518
Adrien Thebo <git@somethingsinistral.net>
2017-02-18 10:39:06 -0800

    boundedrand: document values and methods.

--- lib/include/cygni/util/boundedrand.hpp
+++ lib/include/cygni/util/boundedrand.hpp
@@ -2,19 +2,33 @@
 
 namespace Cygni {
     namespace Util {
+        /**
+         * Randomly walk between values in a bounded range.
+         */
         template <typename T>
         struct BoundedRand {
 
+            /// The range minimum value
             T min;
+            /// The range maximum value
             T max;
+            /// The value to increment/decrement by on each step
             T by;
+            /// The current position in the range
             T val;
+            /// The desired position in the range that we're approaching
             T tgt;
 
             BoundedRand(T min, T max, T by, T val, T tgt) : min(min), max(max), by(by), val(val), tgt(tgt) {}
             BoundedRand(T min, T max, T by, T val) : BoundedRand(min, max, by, val, randomf(max - min) + min) {}
             BoundedRand(T min, T max, T by) : BoundedRand(min, max, by, randomf(max - min) + min) {}
 
+            /**
+             * Take the next step within our range. If the target value is reached then select
+             * a new target value.
+             *
+             * \return The current position in the range.
+             */
             const T & step() {
                 if(val < tgt) {
                     lt();
@@ -29,6 +43,12 @@ namespace Cygni {
                 return val;
             }
 
+        private:
+
+            /**
+             * Take a step towards a target that's less than our current position. If taking the
+             * step would reach or pass the target, reset the target.
+             */
             void lt() {
                 T next = val + by;
 
@@ -40,6 +60,10 @@ namespace Cygni {
                 }
             }
 
+            /**
+             * Take a step towards a target that's greater than our current position. If taking the
+             * step would reach or pass the target, reset the target.
+             */
             void gt() {
                 T next = val - by;
 
