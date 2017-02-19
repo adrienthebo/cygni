3e99f2de7c89ed9bbe51445448cbf15887368a3b
Adrien Thebo <git@somethingsinistral.net>
2017-02-18 16:39:00 -0800

    effects/spectrum: replace Color with HCL, clean up

--- lib/include/cygni/effects/spectrum.hpp
+++ lib/include/cygni/effects/spectrum.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "effect.hpp"
-#include <Color.h>
 
 /**
  * Rotate all LEDs through the hue spectrum.
@@ -8,42 +7,26 @@
 namespace Cygni {
     struct Spectrum : Cygni::Effect {
 
-        Spectrum(Output &output) : Effect(output) {}
+        Spectrum(Output &output) : Effect(output), _hcl(0.0, 0.9, 0.05) {
+        }
 
         virtual void apply() {
-            Color c;
-
-            _hue = next_hue(_hue);
+            _hcl.clamp_hue_by(0.002);
             for(uint32_t idx = 0; idx < _output.size(); idx++) {
-                float current_lum = _lum;
+                HCL tmp(_hcl);
+
                 if(_twinkle_probability % random(_twinkle_probability) == 0) {
-                    current_lum *= 2;
+                    tmp.clamp_lum(tmp.lum() * random(4));
                 }
 
-                float current_sat = _sat;
-                if(_unsat_probability % random(_unsat_probability) == 0) {
-                    current_sat = 0.0;
-                    current_lum /= 2;
-                }
-
-                c.convert_hcl_to_rgb(_hue, current_sat, current_lum);
-                _output.set_pixel(idx, c.red, c.green, c.blue);
+                _output.set_pixel(idx, tmp.to_int());
             }
         }
 
         void call(Environment & env) { apply(); }
     protected:
+        HCL _hcl;
 
-        float next_hue(float in) {
-            float sum = in + 0.002;
-            return sum >= 1.0 ? sum - 1.0 : sum;
-        }
-
-        float _hue = 0.0;
-        float _sat = 0.8;
-        float _lum = 0.1;
-
-        static constexpr int _twinkle_probability = 10000;
-        static constexpr int _unsat_probability = 25000;
+        static constexpr int _twinkle_probability = 2500;
     };
 };
