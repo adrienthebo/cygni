f521a5757b7eec4f7c9fd0c6c42bff7847c9f784
Adrien Thebo <git@somethingsinistral.net>
2017-03-01 19:39:01 -0800

    dmx transmit: rebase

--- exe/dmx-transmit.cpp
+++ exe/dmx-transmit.cpp
@@ -1,20 +1,22 @@
 #include "platform.h"
+#include <TeensyDmx.h>
+
+#include "pin13.h"
+using namespace pin13;
 
 static constexpr uint8_t rs485_enable = 3;
 
-#include <TeensyDmx.h>
-
-#define DMX_REDE 3
-
 byte DMXVal[] = {0xFE};
 
-TeensyDmx Dmx(Serial1, DMX_REDE);
+TeensyDmx Dmx(Serial1, rs485_enable);
 
 void setup() {
-  Dmx.setMode(TeensyDmx::DMX_OUT);
+    setup13();
+    Dmx.setMode(TeensyDmx::DMX_OUT);
 }
 
 void loop() {
-  Dmx.setChannels(0, DMXVal, 1);
-  Dmx.loop();
+    Dmx.setChannels(0, DMXVal, 1);
+    Dmx.loop();
+    pulse13(50, 2);
 }
